e775809b0d36751db8c223dea245101982808fbb
akkumis <96519738+akkumis@users.noreply.github.com>
2022-09-06 18:11:17 +0600

    Add files via upload

---
+++ 1akku.py
@@ -0,0 +1,7 @@
+print("Hello")
+c=input("Where do you live?")
+a=input("Your last name,first name?")
+b=input("How old are you?")
+print("Where do you live?",c)
+print("Your last name,first name?",a)
+print("How old are you?",b)
